b66af9dc8e79e803d8910139978e7854c0380cc5
axlrose- <arjkris789@gmail.com>
2026-08-11 17:09:29 +0530

    matching volume notif

--- scripts/volume-display.sh
+++ scripts/volume-display.sh
@@ -3,10 +3,11 @@
 vol=$(awk '{print $2}' <<< $(wpctl get-volume @DEFAULT_SINK@))
 first=$(awk -F "." '{print $1}' <<< $vol)
 second=$(awk -F "." '{print $2}' <<< $vol)
+mute=$(awk '{print $3}' <<< $(wpctl get-volume @DEFAULT_SINK@))
 
 # formatting
 if [[ $first == "0"  ]]; then
-	printf " %s%%" "$second" 
+	printf " %s%% $mute" "$second" 
 else 
-	printf "  %s%s%%" "$first" "$second" 
+	printf "  %s%s%% $mute" "$first" "$second" 
 fi
--- scripts/volume.sh
+++ scripts/volume.sh
@@ -11,4 +11,5 @@ case "$1" in
 		wpctl set-mute @DEFAULT_SINK@ toggle
 esac
 
-notify-send -r 3 "Volume output" "$(wpctl get-volume @DEFAULT_SINK@)"
+notify-send -r 3 "Volume output" "$(volume-display.sh)"
+
