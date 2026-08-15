db98b731c833f45eeb3ef0fdd972c0c6f9430d87
axlrose- <arjkris789@gmail.com>
2026-08-15 13:10:25 +0530

    packages

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 
 # Install packages needed
 sudo pacman -S --noconfirm --needed bash \ 
-	xorg-server \
-	xorg-xinit \
-	vim \
-	kitty \
-	ttf-jetbrains-mono-nerd \
-	starship \
-	rofi \
-	dunst \
-	libnotify \
-	picom \
-	xclip \
-	maim \
-	networkmanager \
-	bluetui \
-	bluez-utils \
-	wireplumber \
-	brightnessctl \
-	feh
+xorg-server \
+xorg-xinit \
+vim \
+kitty \
+ttf-jetbrains-mono-nerd \
+starship \
+rofi \
+dunst \
+libnotify \
+picom \
+xclip \
+maim \
+networkmanager \
+bluetui \
+bluez-utils \
+wireplumber \
+brightnessctl \
+feh
 
 # Xinitrc setup 
 ln -sf ~/dotfiles/xinit/.xinitrc ~/.xinitrc
